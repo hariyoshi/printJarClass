694916962eea01f885eea831f524fd764454ce36
ariyoshi <ariyoshi.hideo@tis.co.jp>
2016-04-14 19:26:17 +0900

    テキストに出力するようにした。

--- src/PrintJarClass.java
+++ src/PrintJarClass.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Enumeration;
 import java.util.HashMap;
@@ -22,37 +23,40 @@ public class PrintJarClass {
     /**
      * メインメソッド
      *
-     * @param args    対象ディレクトリ
+     * @param args    検索対象ディレクトリ,出力ファイルパス
      */
     public static void main(String args[]) {
 
-        if (args.length == 0) {
-            System.out.println("ディレクトリを指定してください。");
+        if (args.length < 2) {
+            System.out.println("検索対象ディレクトリと出力ディレクトリを指定してください。");
             System.exit(1);
         }
 
-        File targetDir = new File(args[0]);
+        File searchDir = new File(args[0]);
+
+        if (!searchDir.exists() || !searchDir.isDirectory()) {
+            System.out.println("正しい検索対象ディレクトリを指定してください。");
+            System.exit(1);
+        }
+
+        File outputPath = new File(args[1]);
         Map<String, HashMap<String,String>> pomInfo = new HashMap<String, HashMap<String,String>>();
 
-        if (!targetDir.isDirectory()) {
-            System.out.println("ディレクトリを指定してください。");
-            System.exit(1);
-        }
+        readPom(searchDir, pomInfo);
+        print(searchDir, outputPath, pomInfo);
 
-        readPom(targetDir, pomInfo);
-        print(targetDir, pomInfo);
-
+        System.out.println("処理が終了しました。");
     }
 
     /**
      * ディレクトリを再帰検索してpomファイルからgroupId,artifactId,versionを取得する
      *
-     * @param targetDir    対象ディレクトリ
+     * @param searchDir    検索対象ディレクトリ
      * @param pomInfo      pomファイルの情報
      */
-    private static void readPom(final File targetDir, Map<String, HashMap<String,String>> pomInfo) {
+    private static void readPom(final File searchDir, Map<String, HashMap<String,String>> pomInfo) {
 
-        for (File targetFile : targetDir.listFiles()) {
+        for (File targetFile : searchDir.listFiles()) {
             if (targetFile.isDirectory() ) {
             	readPom(targetFile, pomInfo);
             }
@@ -68,7 +72,6 @@ public class PrintJarClass {
                     map.put("artifactId", artifactId);
                     map.put("version", version);
                     pomInfo.put(targetFile.getName().substring(0, targetFile.getName().lastIndexOf(".pom")), map);
-
                 } catch (SAXException | IOException | ParserConfigurationException e) {
                     e.printStackTrace();
                 }
@@ -79,27 +82,29 @@ public class PrintJarClass {
     /**
      * ディレクトリを再帰検索してjarファイル内のクラス情報を一覧を表示する
      *
-     * @param targetDir    対象ディレクトリ
+     * @param searchDir    検索対象ディレクトリ
+     * @param outputPath    出力対象パス
      * @param pomInfo      pomファイルの情報
      */
-    private static void print(final File targetDir, final Map<String, HashMap<String,String>> pomInfo) {
+    private static void print(final File searchDir, final File outputPath, final Map<String, HashMap<String,String>> pomInfo) {
 
-        for (File targetFile : targetDir.listFiles()) {
+        for (File targetFile : searchDir.listFiles()) {
             if (targetFile.isDirectory() ) {
-                print(targetFile, pomInfo);
+                print(targetFile, outputPath, pomInfo);
             } else if (targetFile.getName().endsWith(".jar")) {
                 JarFile jarFile = null;
+                FileWriter writer = null;
                 try {
                     jarFile = new JarFile(targetFile.getAbsolutePath());
+                    writer = new FileWriter(outputPath, true);
                     for (Enumeration<JarEntry> e = jarFile.entries(); e.hasMoreElements();) {
                         JarEntry entry = e.nextElement();
                         if (!entry.isDirectory() && entry.getName().endsWith(".class") && !entry.getName().contains("$")) {
-                          String className = entry.getName().replaceAll("/", ".").replace(".class", "");
-                          HashMap<String, String> map = pomInfo.get(
+                            String className = entry.getName().replaceAll("/", ".").replace(".class", "");
+                            HashMap<String, String> map = pomInfo.get(
                                 targetFile.getName().substring(0, targetFile.getName().lastIndexOf(".jar")));
-
-                            System.out.printf("%s\t%s\t%s\t%s%n",
-                                           map.get("groupId"), map.get("artifactId"), map.get("version"), className);
+                            writer.write(map.get("groupId") + "\t" + map.get("artifactId") + "\t" +
+                                          map.get("version") + "\t" + className + "\r\n");
                         }
                     }
                 } catch (IOException e) {
@@ -109,6 +114,12 @@ public class PrintJarClass {
                     if (jarFile != null) {
                         try {
                             jarFile.close();
+                        } catch (IOException e) {
+                        }
+                    }
+                    if (writer != null) {
+                        try {
+                            writer.close();
                         } catch (IOException e) {
                         }
                     }
